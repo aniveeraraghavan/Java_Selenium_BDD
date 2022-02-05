9a86cb173835202312b043138d250b059985a7e6
Anitha V <anitha.v@healthasyst.com>
2022-02-05 16:14:06 +0530

    session work and assignments

--- SeleniumConcept/src/com/ha/openemr/MessageTest.java
+++ SeleniumConcept/src/com/ha/openemr/MessageTest.java
@@ -3,6 +3,7 @@ package com.ha.openemr;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -33,13 +34,30 @@ public class MessageTest {
 	s1.selectByVisibleText("English (Indian)");
 	
 	driver.findElement(By.xpath("//button[@type= 'submit']")).click();
-	String actualTitle=driver.getTitle();
-	System.out.println(actualTitle);
+//	String actualTitle=driver.getTitle();
+//	System.out.println(actualTitle);
+//	
+//	String url = driver.getCurrentUrl();
+//	System.out.println(url);
+	// this is java script --document.querySelector("li[data-bind='click: logout']");
+	driver.findElement(By.xpath("//div[text()= \"Patient/Client\"]")).click();
+	driver.findElement(By.xpath("//div[text()= \"Patients\"]")).click();
+	driver.switchTo().frame(driver.findElement(By.name("fin")));
+	driver.findElement(By.id("//button[@id= \"create_patient_btn1\"]")).click();
+	driver.switchTo().defaultContent();
+	driver.switchTo().frame(driver.findElement(By.name("pat")));
+	driver.findElement(By.id("form_fname")).sendKeys("Anitha");
+	driver.findElement(By.id("form_lname")).sendKeys("Anith");
 	
-	String url = driver.getCurrentUrl();
-	System.out.println(url);
-	 
-	 
+	
+			
+	
+	//JavascriptExecutor js=(JavascriptExecutor) driver;
+
+//	js.executeScript("document.querySelector(\"li[data-bind='click: logout']\").click();");
+//	driver.switchTo().frame(driver.findElement(By.id("logoutinnerframe")));
+//	driver.findElement(By.cssSelector("menuLabel dropdown-toggle oe-dropdown-toggle")).click();
+	
 	 
 	
 }
--- SeleniumConcept/src/com/ha/seleniumassignments/salesForceSiteTest.java
+++ SeleniumConcept/src/com/ha/seleniumassignments/salesForceSiteTest.java
@@ -21,15 +21,23 @@ public class salesForceSiteTest {
 		 Select s1 = new Select(driver.findElement(By.name("UserTitle")));
 		 s1.selectByVisibleText("IT Manager");
 		 
-		 driver.findElement(By.name("CompanyName")).sendKeys("HA");
+		
 		Select s2= new Select(driver.findElement(By.name("CompanyEmployees")));
 		s2.selectByVisibleText("101 - 500 employees");
 		Select s3= new Select(driver.findElement(By.name("CompanyCountry")));
 		s2.selectByVisibleText("101 - 500 employees");
 		 s3.selectByVisibleText("United Kingdom");
-		//driver.findElement(By.xpath("//div[@class='checkbox-ui']")).click();
-		 driver.findElement(By.xpath("//button[text()='start my free trial']")).click();
-		 //driver.findElement(By.)
+		 driver.findElement(By.name("CompanyName")).sendKeys("HA");
+		 driver.findElement(By.xpath("//div[@class='checkbox-ui']")).click();
+		//below is also the valid xpath. there we have the 1 to decide which one to get.
+		// driver.findElement(By.xpath("(//div[contains(@class,'checkbox-ui')])[1]")).click();
+		//after getting the country we get a tool tip. so this hides the i accept check box. so we fill the company next to country.
+	     driver.findElement(By.xpath("//button[normalize-space()='start my free trial']")).click();
+		 
+		 String error = driver.findElement(By.xpath("")).getText();
+	System.out.println(error);
+	
+	driver.quit();
 	}
 
 }
